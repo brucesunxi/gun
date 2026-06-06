18ff0df821e23ea6cf8739d1b11843fa6fa05f91
brucesunxi <sunxi232@hotmail.com>
2026-06-06 11:40:29 +0800

    Add PWA support for mobile app installation
    - Add manifest.json with app configuration
    - Add Service Worker for offline caching
    - Generate app icons in multiple sizes
    - Update index.html with PWA meta tags

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'battlefield-shooter-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
